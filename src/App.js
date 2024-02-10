1659f1b9c1f8766fc7ee6ca2ef6be305f7d43070
ankit-chakraborty <ankitguitar117@gmail.com>
2024-02-10 20:45:43 +0530

    modifications

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import './index.css'
+import Cards from "./components/Cards"
 
 function App() {
   // react hooks--> created using useState
@@ -24,14 +26,23 @@ function App() {
       setCounter(counter)
     }
   }
+
+  let user={
+    username: "github",
+    password: 1234
+  }
   return (
     <div className="App">
       {/* <h1>the entered username is:- {username}</h1> */}
       {/* the above line of code is the syntax to inject a variable in react */}
-      <h1>the current value is {counter}</h1>
+      <h1 className="text-3xl  bg-green-500 font-bold underline">the current value is {counter}</h1>
       <button onClick={addValue}>Add Value {counter}</button>
       <br/>
       <button onClick={decreaseValue}>Remove Value {counter}</button>
+      <h1 className="bg-green-700">Tailwinf css</h1>
+      {/* you can go to site of :- devui.io to get more of design codes */}
+      <Cards name="github mountains"/>
+      <Cards name="codeblock" someobj={user} para="lorem ipsumvgb vuvba;eur uva;eourn;g"/>
     </div>
   );
 }
